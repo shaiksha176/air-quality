ce70840be217e68244142fa1927fc56078fe54cc
shaiksha vali <iamshaiksha17@gmail.com>
2021-07-11 17:55:27 +0530

    deploying changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
@@ -13,6 +14,10 @@ app.use("/api/weather", weather_router);
 app.use("/api/aqi/latest", aqi_router);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 app.listen(port, () => console.log("server is running on port " + port));
